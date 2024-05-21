a3622256362641cf6354f52fb477173686d90b33
AngelVeraHerrera <angeldelaveracruz@gmail.com>
2024-05-21 12:05:18 +0000

    refactor: new header for the project.

--- LibZMQUtils/includes/LibZMQUtils/PublisherSubscriber/data/publisher_subscriber_data.h
+++ LibZMQUtils/includes/LibZMQUtils/PublisherSubscriber/data/publisher_subscriber_data.h
@@ -1,15 +1,19 @@
 /***********************************************************************************************************************
  *   LibZMQUtils (ZeroMQ High-Level Utilities C++ Library).                                                            *
  *                                                                                                                     *
- *   A modern open-source C++ library with high-level utilities based on the well-known ZeroMQ open-source universal   *
- *   messaging library. Includes custom command based server-client and publisher-subscriber with automatic binary     *
- *   serialization capabilities, specially designed for system infraestructure. Developed as a free software under the *
- *   context of Degoras Project for the Spanish Navy Observatory SLR station (SFEL) in San Fernando and, of course,    *
- *   for any other station that wants to use it!                                                                       *
+ *   A modern open-source and cross-platform C++ library with high-level utilities based on the well-known ZeroMQ      *
+ *   open-source universal messaging library. Includes a suite of modules that encapsulates the ZMQ communication      *
+ *   patterns as well as automatic binary serialization capabilities, specially designed for system infraestructure.   *
+ *   The library is suited for the quick and easy integration of new and old systems and can be used in different      *
+ *   sectors and disciplines seeking robust messaging and serialization solutions.                                     *
+ *                                                                                                                     *
+ *   Developed as free software within the context of the Degoras Project for the Satellite Laser Ranging Station      *
+ *   (SFEL) at the Spanish Navy Observatory (ROA) in San Fernando, Cádiz. The library is open for use by other SLR     *
+ *   stations and organizations, so we warmly encourage you to give it a try and feel free to contact us anytime!      *
  *                                                                                                                     *
  *   Copyright (C) 2024 Degoras Project Team                                                                           *
  *                      < Ángel Vera Herrera, avera@roa.es - angeldelaveracruz@gmail.com >                             *
- *                      < Jesús Relinque Madroñal >                                                                    *                                                            *
+ *                      < Jesús Relinque Madroñal >                                                                    *
  *                                                                                                                     *
  *   This file is part of LibZMQUtils.                                                                                 *
  *                                                                                                                     *
@@ -78,7 +82,7 @@ enum class OperationResult : ResultType
     MSG_OK                 = 0,   ///< All the operation was ok (publish data or receive the data).
     INTERNAL_ZMQ_ERROR     = 1,   ///< An internal ZeroMQ error occurred.
     EMPTY_MSG              = 2,   ///< The message is empty.
-    PUB_NOT_REGISTERED     = 3,   ///< The publisher is not registered in the client.
+    PUB_NOT_REGISTERED     = 3,   ///< The publisher is not registered in the client.                               TODO
     EMPTY_PARAMS           = 6,   ///< The command parameters are missing or empty.
     INVALID_PARTS          = 8,   ///< The message has invalid parts.
     INVALID_MSG            = 10,  ///< The message is invalid.
